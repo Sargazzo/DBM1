6d18bb5bc8fc99448b65dc699985246afddfa846
Sargazzo <62848824+Sargazzo@users.noreply.github.com>
2021-11-08 13:08:28 +0000

    Queries complete (I think)

--- Queries.sql
+++ Queries.sql
@@ -1,7 +1,7 @@
 -- First Query -- Katharina corrected it
-SELECT sport_code, COUNT(DISTINCT(id)) AS participants
-FROM enrolled
-GROUP BY sport_code
+SELECT name, sport_code, COUNT(DISTINCT(id)) AS participants
+FROM enrolled NATURAL JOIN sport
+GROUP BY (name,sport_code)
 ORDER BY participants DESC;
 
 SELECT name,(male_num + female_num) AS num_of_athletes
@@ -51,6 +51,9 @@ SELECT first_name, COUNT(first_name)
     ORDER BY first_name DESC
     );
 
+CREATE INDEX first_name_index ON person USING hash (first_name);
+DROP INDEX first_name_index;
+
 -- Third query -- Check - each
 
 SELECT name, gold_medal_count, silver_medal_count, bronze_medal_count
@@ -67,6 +70,11 @@ ORDER BY last_name;
 SELECT COUNT(p.id) as num_of_athletes
 FROM person as p NATURAL JOIN athlete as a
 WHERE EXTRACT (YEAR FROM a.date_of_birthday) BETWEEN 1980 AND 1990;
+
+-- Indexing B-Tree
+
+CREATE INDEX birthday ON athlete USING btree (date_of_birthday);
+DROP index birthday;
 
 -- Fifth Query -- Check
 SELECT EXTRACT (YEAR FROM date_of_birthday)
@@ -138,7 +146,7 @@ FROM (person NATURAL JOIN athlete NATURAL JOIN medalists) AS winner
 WHERE winner.gender = 'F' AND winner.id IN(
     SELECT id
     FROM medalists AS m
-    WHERE m.id = winner.id AND m.year = winner.year AND m.sport_code != winner.sport_code
+    WHERE m.id = winner.id AND m.year != winner.year AND m.sport_code != winner.sport_code
     )
 ORDER BY winner.id;
 
@@ -151,12 +159,12 @@ SELECT DISTINCT(init.id), init.first_name,init.last_name,init.sport_code
 FROM (person NATURAL JOIN athlete NATURAL JOIN enrolled) AS init
 WHERE init.id IN(
         SELECT e.id
-        FROM (SELECT id FROM enrolled WHERE year = init.year) AS e -- Enrolled in the first year
-            INNER JOIN (SELECT id from medalists WHERE year = init.year + 8) AS win -- Winner 3rd edition
+        FROM (SELECT id FROM enrolled WHERE year = init.year AND sport_code = init.sport_code) AS e -- Enrolled in the first year
+            INNER JOIN (SELECT id from medalists WHERE year = init.year + 8 AND sport_code = init.sport_code) AS win -- Winner 3rd edition
             ON e.id = win.id
             EXCEPT(SELECT m.id -- Not medalist in the first year
                FROM medalists AS m
-               WHERE m.year = init.year)
+               WHERE m.year = init.year AND m.sport_code = init.sport_code)
         )
     AND init.id NOT IN( -- not enrolled in the 2nd edition
         SELECT id
@@ -166,6 +174,33 @@ WHERE init.id IN(
 ORDER BY init.id ASC;
 
 
+-- Index to accelarate querie
+CREATE INDEX enrolled_index ON enrolled USING hash (year);
+CREATE INDEX medalists_index ON medalists USING hash (year);
+DROP index enrolled_index;
+DROP index medalists_index;
+
 INSERT INTO enrolled VALUES('JUD',1,2028);
 INSERT INTO enrolled VALUES('JUD',1,2024);
+DELETE FROM enrolled where id = 1 AND year = 2024;
 INSERT INTO medalists VALUES('JUD','Men -100 kg',1,'Gold', 2028);
+DELETE FROM medalists where id = 1 AND year = 2028;
+
+INSERT INTO enrolled VALUES('ARC',1,2028);
+INSERT INTO medalists VALUES('ARC','Men''s Individual - W1',1,'Gold', 2028);
+
+SELECT * FROM medalists where id = 1;
+SELECT * FROM enrolled where id = 1;
+
+-- Test 7th query
+INSERT INTO enrolled VALUES('WBK',8,2012);
+INSERT INTO medalists VALUES('WBK','Women',8,'Gold', 2012);
+
+INSERT INTO enrolled VALUES('WBK',8,2014);
+INSERT INTO medalists VALUES('WBK','Women',8,'Gold', 2014);
+
+INSERT INTO enrolled VALUES('ARC',8,2014);
+INSERT INTO medalists VALUES('ARC','Women''s Individual - W1',8,'Gold', 2014);
+
+
+
