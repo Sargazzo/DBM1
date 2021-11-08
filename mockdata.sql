b563ed12089fefb86e83319682ef85471866ff45
katharinaalefs <77345153+katharinaalefs@users.noreply.github.com>
2021-11-08 21:44:15 +0100

    more mock data

--- mockdata.sql
+++ mockdata.sql
@@ -2,7 +2,7 @@
 INSERT INTO enrolled VALUES('JUD',1,2028);
 INSERT INTO medalists VALUES('JUD','Men -100 kg',1,'Gold', 2028);
 
-INSERT person
+INSERT 
 
 -- Test 7th query
 INSERT INTO enrolled VALUES('WBK',8,2012);
@@ -10,3 +10,15 @@ INSERT INTO medalists VALUES('WBK','Women',8,'Gold', 2012);
 
 INSERT INTO enrolled VALUES('ARC',8,2014);
 INSERT INTO medalists VALUES('ARC','Women''s Individual - W1',8,'Gold', 2014);
+
+INSERT INTO enrolled VALUES('JUD',2731,2028);
+INSERT INTO medalists VALUES('JUD','Women -100 kg',2731,'Gold', 2028);
+
+INSERT INTO enrolled VALUES('JUD',2731,2028);
+INSERT INTO medalists VALUES('JUD','Women -100 kg',2731,'Gold', 2028);
+
+INSERT INTO enrolled VALUES('JUD',3811,2024);
+INSERT INTO medalists VALUES('JUD','Women -100 kg',3811,'Gold', 2024);
+
+INSERT INTO enrolled VALUES('JUD',2417,2016);
+INSERT INTO medalists VALUES('JUD','Women -100 kg',2417,'Gold', 2016);
